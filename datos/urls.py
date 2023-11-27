4f6fdd5a609cfb559bd9ebdf297411721ab31842
pachomejia26 <pachomejia26@gmail.com>
2023-11-27 00:24:16 -0500

    recomendacion_juego_terminado

--- datos/urls.py
+++ datos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('play-time-genre/<str:genero>/', views.PlayTimeGenre, name='play_time_genre'),
+    path('recomendaciones/<str:id_producto>/', views.recomendacion_juego, name='recomendaciones')
     ]
--- datos/views.py
+++ datos/views.py
@@ -3,12 +3,19 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from secrets import token_hex
 
-from .helpers import getAnioPlayTimeGenre
+from .helpers import getAnioPlayTimeGenre,getRecomendaciones
 
 @api_view(['GET'])
 def PlayTimeGenre(request, genero: str):
+    genero=genero.capitalize()
     anio = getAnioPlayTimeGenre(genero)
     data = {"Anio de lanzamiento con mas horas jugadas para Genero {}".format(genero) : anio}
     return JsonResponse(data, status=status.HTTP_200_OK)
     # Si se ingresa un genero que no existe:
     #return JsonResponse({"message": "genero no existe"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def recomendacion_juego(request, id_producto: str):
+    juegos_recomendados= getRecomendaciones(id_producto)
+    data = {'cinco recomendaciones':juegos_recomendados}
+    return JsonResponse(data,status=status.HTTP_200_OK)
